b5b73eda1fc5846df25fd278b9cbe99e1c8f8790
Erick Maeda <erick.maeda26@gmail.com>
2018-10-18 01:13:49 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Products from './components/products';
+import ProductRow from './components/productRow';
+import {
+  getProducts
+} from './api/products';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: getProducts(),
+      order: [],
+      nextId: 3,
+      query: ''
+    };    
+
+    this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
+  }
+
+  addToOrder(product) {
+    let order = this.state.order;
+    order.push(product);    
+    this.setState({
+      order  
+    });
+  }
+
+  removeFromOrder(index) {
+    this.setState({
+        order: this.state.order.filter((order, indexFilter) => index != indexFilter)
+      });
+  }
+
+  calculateTotal() {
+    let value = 0;
+    let order = this.state.order;
+    order.forEach((element) => value += element.value);
+    return value;
+  }
+
+  handleSearchChanged = () => {
+    this.setState({
+      query: this.search.value
+    });
+  } 
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="orders-wrapper">
+          <Header total={this.calculateTotal()}/>
+          <input 
+            ref={input => this.search = input}
+            onChange={this.handleSearchChanged}
+            className="search-input" 
+            type="text" 
+            placeholder="Search on your order"
+          />
+          <Products addToOrder={this.addToOrder} products={this.state.products}/>
+          <ul>
+            {
+              this.state.order.filter((product) => {
+                const query = this.state.query.toLowerCase();
+                return (
+                  query.trim() == '' || 
+                  product.description.toString().toLowerCase().includes(query) ||
+                  product.value.toString().toLowerCase().includes(query)
+                );
+              }).map((product, index) => {                
+                return <ProductRow product={product} key={product.index} id={product.id} removeFromOrder={() => this.removeFromOrder(index)}/>
+              })
+            }
+          </ul>
+        </div>
       </div>
     );
   }
